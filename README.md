921173ba4bbbf599bd7205f67d3ff6c0266c4e92
TS-SAHANA <sahanats4@gmail.com>
2023-09-30 21:43:55 +0530

    aisummarizer

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Workshop-Project-2-AISummarizer
+
+### Installation
+
+1. Get a free API Key at [https://huggingface.co/](https://huggingface.co/)
+1. Clone the repo
+   ```sh
+   git clone https://github.com/NoumaanAhamed/Workshop-Project-2-AISummarizer.git
+   ```
+2. Install NPM packages
+
+- In First Terminal
+   ```sh
+   cd backend
+   npm install
+   ```
+- In Second Terminal
+   ```sh
+   cd frontend
+   npm install
+   ```
+3. Inside `backend` Folder ,create a new `.env` file and copy from `.env.example` 
+   ```js
+   HUGGINGFACE_API_TOKEN='YOUR_HUGGINGFACE_API_TOKEN'
+   ```
+4. Start both the Frontend and Backend
+- In First Terminal (inside backend)
+   ```sh
+   node index.js
+   ```
+- In Second Terminal(inside frontend)
+   ```sh
+   npm run dev
+   ```
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/summarize', async (req, res) => {
+  try {
+    const apiUrl = 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn';
+    const result = await axios.post(apiUrl, req.body, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + process.env.HUGGINGFACE_API_TOKEN,
+      },
+    });
+
+    res.json(result.data[0].summary_text);
+  } catch (error) {
+    console.error('Error summarizing text:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
